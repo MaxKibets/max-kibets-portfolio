426e91de7d41be9e495a4428027aebd75787b99b
MaxKibets <kibetss3@gmail.com>
2025-01-07 13:56:28 +0200

    Group component

---
+++ components/ui/Group/Group.tsx
@@ -0,0 +1,11 @@
+import { FC, Children } from "react";
+
+import { WithChildren } from "@/types/shared";
+
+import css from "./group.module.css";
+
+const Group: FC<WithChildren> = ({ children }) => (
+  <div className={css.group}>{children}</div>
+);
+
+export default Group;
