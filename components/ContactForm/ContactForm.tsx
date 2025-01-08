a80edb86309b586e777ca554aa2729a967788b91
MaxKibets <kibetss3@gmail.com>
2025-01-08 17:19:45 +0200

    ContactFrom component

---
+++ components/ContactForm/ContactForm.tsx
@@ -0,0 +1,21 @@
+import { Input, Label, Textarea } from "@/components/ui";
+
+const ContactForm = () => (
+  <form>
+    <div>
+      <Label htmlFor="name">Name</Label>
+      <Input type="text" id="name" />
+    </div>
+    <div>
+      <Label htmlFor="email">Email</Label>
+      <Input type="email" id="email" />
+    </div>
+    <div>
+      <Label htmlFor="message">Message</Label>
+      <Textarea id="message" />
+    </div>
+    <button type="submit">Send</button>
+  </form>
+);
+
+export default ContactForm;
