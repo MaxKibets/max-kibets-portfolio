6ee041fb9e51606bf290f3b40bb3ff14e05d0cc7
MaxKibets <kibetss3@gmail.com>
2025-01-08 17:15:41 +0200

    prettier fix

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,19 @@
+import { CommentedRows } from "@/components/ui";
+import ContactForm from "@/components/ContactForm";
+
 const ContactPage = () => {
-  return <div>Contact PAGE</div>;
+  return (
+    <>
+      <CommentedRows>
+        <h2>GET IN TOUCH</h2>
+        <p>
+          Let’s connect! Whether you have a project in mind or just want to say hello, I’m
+          always happy to chat.
+        </p>
+      </CommentedRows>
+      <ContactForm />
+    </>
+  );
 };
 
 export default ContactPage;
