21dbc63861acff53f674b2136cc14181b1a8388e
MaxKibets <kibetss3@gmail.com>
2025-01-08 10:29:05 +0200

    Sidebar opened state for mob

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,13 +1,21 @@
 "use client";
 
-import { useToggle } from "@/hooks/useToggle";
+import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 
 import SidebarLayout from "./SidebarLayout";
 
 const Sidebar = () => {
-  const [expanded, toggleExpanded] = useToggle(false);
+  const isMob = useMediaQuery({ query: "(max-width: 1024px)" });
+  const [expanded, setExpanded] = useState(false);
 
-  return <SidebarLayout expanded={expanded} onFileClick={toggleExpanded} />;
+  useEffect(() => {
+    setExpanded(!isMob);
+  }, [isMob]);
+
+  return (
+    <SidebarLayout expanded={expanded} onFileClick={() => setExpanded((prev) => !prev)} />
+  );
 };
 
 export default Sidebar;
