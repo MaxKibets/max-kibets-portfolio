f16d6fed7ea409a126e07dc8aae597ac52d9b64f
MaxKibets <kibetss3@gmail.com>
2025-02-06 16:52:35 +0200

    bundle-analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,28 @@
 import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.optimization.splitChunks = {
+      chunks: "all",
+      maxSize: 170 * 1024,
+    };
+    // config.optimization.splitChunks = {
+    //   cacheGroups: {
+    //     nextClient: {
+    //       test: /[\\/]node_modules[\\/]next[\\/]dist[\\/]client[\\/]/,
+    //       name: "next-client",
+    //       chunks: "initial",
+    //     },
+    //     nextCompiled: {
+    //       test: /[\\/]node_modules[\\/]next[\\/]dist[\\/]compiled[\\/]/,
+    //       name: "next-compiled",
+    //       chunks: "initial",
+    //     },
+    //   },
+    // };
+    return config;
+  },
   experimental: {
     staleTimes: {
       dynamic: 300, // 5 minutes do not refetch page data
@@ -8,4 +30,9 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const withBundleAnalyzerConfig = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+  openAnalyzer: true,
+});
+
+export default withBundleAnalyzerConfig(nextConfig);
